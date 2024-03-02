2bdc7933f59a15663f8c7674039106addc09ca82
Miguel Figueroa <91326484+MiguelFOlivar@users.noreply.github.com>
2024-03-01 18:55:10 -0600

    Version incial de proyecto: CRUD-employees

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavBar() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" bg="primary" data-bs-theme="light">
+      <Container fluid>
+        <Navbar.Brand href="#">CRUD Empleados</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+          ></Nav>
+          <Form className="d-flex">
+            <Button variant="outline-success">Agregar Empleado</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
